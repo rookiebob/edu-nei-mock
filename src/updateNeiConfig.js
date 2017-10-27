f1fd2df51fb25982a4eea90910a753d94624b7dd
hzchenbo2014 <hzchenbo2014@corp.netease.com>
2017-10-27 15:29:55 +0800

    add dwr support

--- src/updateNeiConfig.js
+++ src/updateNeiConfig.js
@@ -5,6 +5,7 @@ var path = require('path');
 
 module.exports = function () {
     var pattern = './mock/*/server.config.js';
+    var dwrPattern = './mock/**/*.dwr/data.js';
     var NeiConfigPath , NeiConfig , routes , MOCKPATH;
 
     function main(){
@@ -23,11 +24,12 @@ module.exports = function () {
         MOCKPATH = prog.mockpath;
         console.log("需要代理的路径是 %s" , MOCKPATH);
 
+        //更新nei的配置文件
         glob(pattern, {nodir: true}, function (err, files) {
             if(err){
                 console.log(err);
             }else{
-                if(!files){
+                if(!files || files.length < 1){
                     console.log("请确保server.config.js文件存在。如有问题请重新build当前nei工程");
                     return;
                 }
@@ -39,11 +41,31 @@ module.exports = function () {
             }
 
         });
+
+        //处理本地mock的dwr数据
+        glob(dwrPattern, {nodir: true}, function (err, files) {
+            if(err){
+                console.log(err);
+            }else{
+                if(!files || files.length < 1){
+                    console.log("暂无dwr接口需要替换！");
+                    return;
+                }
+                files.forEach(function(_path){
+                    var dwrPath = path.resolve('./' , _path);
+                    console.log("覆盖的dwr路径：%s" , dwrPath);
+                    updateDwrData(dwrPath)
+                });
+                console.log('配置已经更新完成，愉快的联调吧！');
+
+            }
+
+        });
     }
 
 
 
-
+    //更新nei的配置文件
     function updateNeiConfig(NeiConfig){
 
         routes = {
@@ -76,9 +98,18 @@ module.exports = function () {
         }
         console.log('***********nei配置信息End**************');
 
-        console.log('配置已经更新完成，愉快的联调吧！');
         //console.log(`var path = require('path'); \n module.exports = ${JSON.stringify(NeiConfig,null,4)}`);
         fs.writeFileSync(NeiConfigPath, `var path = require('path'); \n module.exports = ${JSON.stringify(NeiConfig,null,4)}`);
+    }
+
+    //处理本地mock的dwr数据
+    function updateDwrData(dwrInterfacePath){
+
+        var cb = "\`//#DWR-INSER\n//#DWR-REPLY\ndwr.engine._remoteHandleCallback('\${req.body['batchId']}','\${req.body['c0-id']}','\${JSON.stringify(json)}')\`";
+
+        fs.writeFileSync(dwrInterfacePath,`module.exports = function (json, req) {
+        req.res.contentType('text/javascript;charset=UTF-8'); 
+        return ${cb};`)
     }
 
 
